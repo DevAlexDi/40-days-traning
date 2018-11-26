2c7ceba4fdcb786302d7418b3deebb09394db68f
keitmark <keitmark@mail.ru>
2018-11-26 23:22:01 +0300

    index system-home audio

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-
-
+    //sm hide show sidebar
+    
+    $('.system__show-hide-sidebar').click(function(){
+        if(!$('.system__sidebar').hasClass('show-menu')){
+            $('.system__sidebar').addClass('show-menu');
+        }
+    });
   
+    
+    $('.system__content').click(function(){
+        if($('.system__sidebar').hasClass('show-menu')){
+            $('.system__sidebar').removeClass('show-menu');
+        }
+        
+    });
+    
+    
 });
